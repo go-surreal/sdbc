5740cd8ef75466d47a89114ee99e8f38d6d08072
Marc Binz <code@marcbinz.me>
2023-09-19 22:47:35 +0200

    update

--- response.go
+++ response.go
@@ -111,11 +111,6 @@ func (c *Client) handleMessage(data []byte) {
 
 	c.logger.DebugContext(c.connCtx, "Received message.", "res", res)
 
-	if res.Error != nil {
-		c.logger.ErrorContext(c.connCtx, "Received error response.", "error", res.Error)
-		return
-	}
-
 	if res.ID == "" {
 		c.handleLiveQuery(res)
 		return
